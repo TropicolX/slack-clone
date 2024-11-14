ee9464d44f053af1b713aabaaf0df45395f38753
TropicolX <jacobsbusayo@gmail.com>
2024-11-14 13:38:54 +0100

    type temp fix

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,9 +5,12 @@ let prisma: PrismaClient;
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
+  // @ts-expect-error global.prisma is used by @prisma/client
   if (!global.prisma) {
+    // @ts-expect-error global.prisma is used by @prisma/client
     global.prisma = new PrismaClient();
   }
+  // @ts-expect-error global.prisma is used by @prisma/client
   prisma = global.prisma;
 }
 export default prisma;
